d4ac66a9c512a2a30d495c7d724b80597211ca2a
Min Thway Htut <minthwayhtut568@gmail.com>
2025-02-21 12:56:45 +0100

    First release of the project

---
+++ demoproject/demoapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+def index(request):
+    return HttpResponse("Hello, World!")
+
+def homepage(request):
+    return HttpResponse("Hello, this is a home page.")
+
+def display_data(request):
+    data_joined = datetime.today().year
+    return HttpResponse(data_joined)
+
+def testing(request):
+    path = request.path
+    method = request.method
+    content = '''
+<center>
+<h2>Testing Django Request Response Objects</h2>
+<p>Request path: {}</p>
+<p>Request method: {}</p>
+</center>'''.format(path, method)
+    return HttpResponse(content)
+
---
+++ demoproject/demoproject/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from demoapp import views
+
+urlpatterns = [
+    path('', include("demoapp.urls")),
+    path('admin/', admin.site.urls),
+    path('homepage/', views.homepage),
+    path('display_data/', views.display_data),
+    path('testing/', views.testing)
+]
