42e95a06818d0aa60c0c5bb95236b9e8ab7d77fb
Min Thway Htut <minthwayhtut568@gmail.com>
2025-02-22 18:39:00 +0100

    Working with Migrations

--- demoproject/myapp/models.py
+++ demoproject/myapp/models.py
@@ -5,3 +5,7 @@ class Person(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=20)
 
+class Menuitems(models.Model):
+    name = models.CharField(max_length=200)
+    course = models.CharField(max_length=300)
+    year = models.IntegerField()
